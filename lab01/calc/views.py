9923efa0a1795f61cc97d3db0d21d6dca8d39b31
LeonardoC <95712634+LeonardoCoaquira@users.noreply.github.com>
2023-03-24 21:43:45 -0500

    added graphic display

--- lab01/calc/views.py
+++ lab01/calc/views.py
@@ -9,12 +9,13 @@ opers = {
 }
 
 def index(request):
-    return HttpResponse("<h1>Estás dentro de la Calculadora!</h1>")
+    return render(request,'index.html')
 
 def calcular(request, oper, num1, num2):
     try:
         res = eval(f"{num1} {opers[oper]} {num2}")
-        message = f"La {oper} de {num1} {opers[oper]} {num2} = {res}"
+        context = {'num1': num1, 'num2': num2, 'result': res, 'oper' : oper}
+        return render(request, 'calc.html',context)
     except:
         message = "Error al calcular o al ingresar la operación"
-    return HttpResponse(message)
+        return HttpResponse(message)
