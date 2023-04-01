991cafd5f82b40c83fe9c5c802b9f7462932421e
Leonardo Coaquira <coaquiraleonardo19@gmail.com>
2023-03-31 22:31:21 -0500

    calculadora finished

--- lab02/calculadora/views.py
+++ lab02/calculadora/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    context = {
+        'titulo' : "Calculadora",
+    }
+    return render(request,'calculadora/index.html',context)
+
+def calcular(request):
+    if request.method == 'POST':
+        
+        num1 = int(request.POST.get('num1', 0))
+        num2 = int(request.POST.get('num2', 0))
+        operacion = request.POST.getlist('operacion', '')
+        
+        if 'suma' in operacion:
+            resultado = num1 + num2
+        
+        elif 'resta' in operacion:
+            resultado = num1 - num2
+        
+        elif 'multiplicacion' in operacion:
+            resultado = num1 * num2
+        
+        return render(request, 'calculadora/result.html', {'resultado': resultado})
+    return render(request, 'calculadora/index.html')
--- lab02/lab02/settings.py
+++ lab02/lab02/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'cilindro.apps.CilindroConfig',
+    'calculadora.apps.CalculadoraConfig',
     'encuesta.apps.EncuestaConfig',
     'django.contrib.admin',
     'django.contrib.auth',
